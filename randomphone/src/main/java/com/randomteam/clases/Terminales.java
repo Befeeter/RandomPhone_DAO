3ef1e8c8010b839af492ca341bda0fe76c57552d
daniellopezmorales <da.lopezm5@gmail.com>
2017-03-08 13:54:18 +0100

    Terminales

--- randomphone/src/main/java/com/randomteam/clases/Terminales.java
+++ randomphone/src/main/java/com/randomteam/clases/Terminales.java
@@ -1,5 +1,9 @@
-public class Terminales {
-	private Label tituloL;
+package com.randomteam.clases;
+
+import com.randomteam.ventanas.Terminales_ventana;
+
+public class Terminales extends Terminales_ventana{
+	/*private Label tituloL;
 	private Label routerL;
 	private Button routerB;
 	private Label decodificadorL;
@@ -7,7 +11,7 @@ public class Terminales {
 	private Label estadoL;
 	private Button guardarB;
 	public ModificarServiciosCm vModificarServiciosCm;
-	public AltaNuevoCliente vAltaNuevoCliente;
+	public AltaNuevoCliente vAltaNuevoCliente;*/
 
 	public Informar enviarDatos() {
 		throw new UnsupportedOperationException();
