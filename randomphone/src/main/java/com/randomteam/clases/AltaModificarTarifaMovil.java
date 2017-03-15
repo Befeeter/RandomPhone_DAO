ab652589403afd7f62ad241d4d6b34f3eecae226
Befeeter <jorge_zk8@hotmail.com>
2017-03-15 13:35:54 +0100

    Añado ventana Editar Tarifas

--- randomphone/src/main/java/com/randomteam/clases/AltaModificarTarifaMovil.java
+++ randomphone/src/main/java/com/randomteam/clases/AltaModificarTarifaMovil.java
@@ -1,7 +1,11 @@
+package com.randomteam.clases;
+
 public class AltaModificarTarifaMovil extends AltaModificarTarifas {
+	/*
 	private Label minutosL;
 	private TextField minutosTF;
 	private Label datosL;
 	private TextField datosTF;
 	public TarifasMovil vTarifasMovil;
+	*/
 }
