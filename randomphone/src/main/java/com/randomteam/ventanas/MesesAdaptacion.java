40ac837fd379d258c34948665a822a28b74935f0
daniellopezmorales <da.lopezm5@gmail.com>
2017-04-15 21:02:56 +0200

    MesesAdaptacion, sitioWeb

--- randomphone/src/main/java/com/randomteam/ventanas/MesesAdaptacion.java
+++ randomphone/src/main/java/com/randomteam/ventanas/MesesAdaptacion.java
@@ -1,7 +1,9 @@
 package com.randomteam.ventanas;
-public class MesesAdaptacion {
+public class MesesAdaptacion extends MesesAdaptacion_ventana {
+	/*
 	private Label mesesL;
 	private TextField mesesTF;
 	private Button aceptarB;
 	public Tarifa vTarifa;
+	*/
 }
