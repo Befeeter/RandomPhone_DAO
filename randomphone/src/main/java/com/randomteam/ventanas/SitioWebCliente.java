cede585e17cb135dbb2fd50b6481f0ebe491a69d
daniellopezmorales <da.lopezm5@gmail.com>
2017-04-17 20:09:40 +0200

    Cambios sitios web

--- randomphone/src/main/java/com/randomteam/ventanas/SitioWebCliente.java
+++ randomphone/src/main/java/com/randomteam/ventanas/SitioWebCliente.java
@@ -1,6 +1,9 @@
 package com.randomteam.ventanas;
+
+public class SitioWebCliente extends sitioWebCliente_ventana {
+/*
 public class SitioWebCliente extends SitioWeb {
 	private Button miCuenta;
 	public Cliente vCliente;
-	public MiCuenta vMiCuenta;
+	public MiCuenta vMiCuenta;*/
 }
--- randomphone/src/main/java/com/randomteam/ventanas/SitioWebInternauta.java
+++ randomphone/src/main/java/com/randomteam/ventanas/SitioWebInternauta.java
@@ -1,6 +1,9 @@
 package com.randomteam.ventanas;
+
+public class SitioWebInternauta extends sitioWebInternauta_ventana {
+/*
 public class SitioWebInternauta extends SitioWeb {
 	private Button login;
 	public internauta vinternauta;
-	public Login vLogin;
+	public Login vLogin;*/
 }
