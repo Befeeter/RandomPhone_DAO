8695ca5d74108990e2ee69abfa0e6f62cc6e4b54
daniellopezmorales <da.lopezm5@gmail.com>
2017-06-02 19:25:12 +0200

    Probando conexion y consultas con la base de datos

--- randomphone/src/main/java/DB/BD_Canales.java
+++ randomphone/src/main/java/DB/BD_Canales.java
@@ -2,12 +2,52 @@ package DB;
 
 import DB.Canal;
 
+import java.awt.datatransfer.StringSelection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+import com.vaadin.data.provider.Query;
+
 public class BD_Canales {
 	public BD_Principal bD_Principal_canales;
 	public Canal[] canal = new Canal[0];
+	
+	Connection conexion;
+    PreparedStatement ps;
+    ResultSet rs;  
 
 	public Canal[] cargarCanales(int paqueteid) {
-		throw new UnsupportedOperationException();
+		Canal [] canales = null;
+		try {
+			conexion = Conexion.getConnection();
+            ps = conexion.prepareStatement("SELECT * from canal INNER JOIN paquete_canal ON canal.id=paquete_canal.CanalId WHERE paquete_canal.PaqueteId=?");
+            ps.setInt(1, paqueteid);
+            rs = ps.executeQuery();
+            canales = new Canal [rs.getRow()];
+            for (int i=0; i < rs.getRow(); i++) {
+            	System.out.println(i);
+            	Canal canal = (Canal) rs.getObject(i);
+            	canales[i] = canal;
+            	rs.next();
+            }
+            ps.close();
+            conexion.close();
+        } catch (SQLException exception) {
+            //JOptionPane.showMessageDialog(null, "Impossivel registar armazém " + exception, "Armazém", JOptionPane.ERROR_MESSAGE);
+        	System.out.println(exception.getMessage());
+        }
+        return canales;
+	}
+	
+	public static void main (String Args []) {
+		BD_Canales canales = new BD_Canales();
+		Canal[] canalsefs = canales.cargarCanales(1);
+		for (Canal canal : canalsefs) {
+			System.out.println(canal);
+		}
 	}
 
 	public boolean crearCanal(Canal canal) {
