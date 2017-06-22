f4a2a27b865b16977c09b338ecbb3f4824d4597e
daniellopezmorales <da.lopezm5@gmail.com>
2017-06-22 19:13:33 +0200

    Solucion servicios cliente

--- randomphone/src/main/java/com/randomteam/ventanas/ModificarServicios.java
+++ randomphone/src/main/java/com/randomteam/ventanas/ModificarServicios.java
@@ -40,6 +40,18 @@ public class ModificarServicios extends ModificarServicios_ventana {
 	private ArrayList<Servicio> sFijoD = new ArrayList<>();
 	private ArrayList<Servicio> sFibraD = new ArrayList<>();
 	private ArrayList<Servicio> sTvD = new ArrayList<>();
+	
+	// para controlar si se ha modificado el servicio
+	private boolean modMovil=false;
+	private boolean modFijo=false;
+	private boolean modFibra=false;
+	private boolean modTelevision=false;
+	
+	// para guardar los servicios actuales
+	private Servicio movilActual;
+	private Servicio fijoActual;
+	private Servicio fibraActual;
+	private Servicio televisionActual;
 
 	public ModificarServicios() {
 		Page.getCurrent().getStyles().add(".form-template{overflow: hidden !important;}");
@@ -67,16 +79,20 @@ public class ModificarServicios extends ModificarServicios_ventana {
 		for (Servicio servicio : servicios) {
 			if (servicio.getNombre().contains("Movil")) {
 				movilCB.setValue(servicio);
+				movilActual=servicio;
 			}
 
 			if (servicio.getNombre().contains("Fijo")) {				
 				fijoCB.setValue(servicio);
+				fijoActual=servicio;
 			}
 			if (servicio.getNombre().contains("Fibra")) {
 				fibraCB.setValue(servicio);
+				fibraActual=servicio;
 			}
 			if (servicio.getNombre().contains("Televisión")) {
 				televisionCB.setValue(servicio);
+				televisionActual=servicio;
 			}
 		}
 
@@ -131,27 +147,55 @@ public class ModificarServicios extends ModificarServicios_ventana {
 			televisionCB.setItems(sTvD);
 		}
 		
+		movilCB.addValueChangeListener(ClickEvent -> {
+			modMovil=true;
+		});
+		
+		fijoCB.addValueChangeListener(ClickEvent -> {
+			modFijo=true;
+		});
+		
+		fibraCB.addValueChangeListener(ClickEvent -> {
+			modFibra=true;
+		});
+		
+		televisionCB.addValueChangeListener(ClickEvent -> {
+			modTelevision=true;
+		});
+		
 		//Botón Aceptar
 		aceptarB.addClickListener(ClickEvent->{
 			ArrayList<Servicio> nServicios = new ArrayList<>();
-			if (!movilCB.isEmpty())
+			if (!movilCB.isEmpty()&&modMovil)
 				nServicios.add(movilCB.getValue());
-			if(!fijoCB.isEmpty())
+			else if (!movilCB.isEmpty())
+				nServicios.add(movilActual);
+			if(!fijoCB.isEmpty()&&modFijo)
 				nServicios.add(fijoCB.getValue());
-			if(!fibraCB.isEmpty())
+			else if (!fijoCB.isEmpty())
+				nServicios.add(fijoActual);
+			if(!fibraCB.isEmpty()&&modFibra)
 				nServicios.add(fibraCB.getValue());
-			if(!televisionCB.isEmpty())
+			else if (!fibraCB.isEmpty())
+				nServicios.add(fibraActual);
+			if(!televisionCB.isEmpty()&&modTelevision)
 				nServicios.add(televisionCB.getValue());
+			else if (!televisionCB.isEmpty())
+				nServicios.add(televisionActual);
 			servicios = nServicios.toArray(new Servicio[nServicios.size()]);
 			int idFactura = c.getFactura()[0].getId();
-			if(iC.modificarServicios(servicios, idFactura)){
-				Notification.show("Servicios Actualizados Correctamente");
+			if (modMovil||modFijo||modFibra||modTelevision) {
+				if(iC.modificarServicios(servicios, idFactura)){
+					Notification.show("Servicios Actualizados Correctamente");
+					this.removeAllComponents();
+					this.addComponent(new ModificarServicios());
+				}
+				else
+					Notification.show("Ups Algo fue mal, habla con un Administrador");
+			} else {
 				this.removeAllComponents();
 				this.addComponent(new ModificarServicios());
 			}
-			else
-				Notification.show("Ups Algo fue mal, habla con un Administrador");
-			
 		});
 
 		// Botón Cancelar.
